e2e750383a6a8cdc771574f298cbe44ab8c55505
Oleg Barbashow <teosofio@gmail.com>
2022-01-24 15:52:03 +0200

    Opening a gift window by scroll to the end of the page. Made modal gift window

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,5 +1,7 @@
 const modals = () => {
-  function bindModal(triggerSelector, modalSelector, closeSelector, closeClickOverlay = true) {
+    let btnPressed = false;
+
+  function bindModal(triggerSelector, modalSelector, closeSelector, destroyTrigger = false) {
       const trigger = document.querySelectorAll(triggerSelector),
           modal = document.querySelector(modalSelector),
           close = document.querySelector(closeSelector),
@@ -12,8 +14,15 @@ const modals = () => {
                  e.preventDefault();
              }
 
+             btnPressed = true;
+
+             if (destroyTrigger) {
+                 item.remove();
+             }
+
              windows.forEach(item => {
                 item.style.display = 'none';
+                item.classList.add('animated', 'fadeIn');
              });
 
              modal.style.display = 'block';
@@ -33,7 +42,7 @@ const modals = () => {
       });
 
       modal.addEventListener('click', (e) => {
-         if (e.target === modal && closeClickOverlay) {
+         if (e.target === modal) {
              windows.forEach(item => {
                  item.style.display = 'none';
              });
@@ -56,6 +65,7 @@ const modals = () => {
           });
 
           if (!display) {
+              let scroll = calcScroll();
               document.querySelector(selector).style.display = 'block';
               document.body.style.overflow = 'hidden';
               document.body.style.marginRight = `${scroll}px`;
@@ -76,8 +86,19 @@ const modals = () => {
       return scrollWidth;
   }
 
+  function openByScroll(selector) {
+      window.addEventListener('scroll', () => {
+          let scrollHeight = Math.max(document.documentElement.scrollHeight, document.body.scrollHeight);
+          if (!btnPressed && (window.pageYOffset + document.documentElement.clientHeight >= scrollHeight)) {
+            document.querySelector(selector).click();
+          }
+      });
+  }
+
   bindModal('.button-design', '.popup-design', '.popup-design .popup-close');
   bindModal('.button-consultation', '.popup-consultation', '.popup-consultation .popup-close');
+  bindModal('.fixed-gift', '.popup-gift', '.popup-gift .popup-close', true);
+  openByScroll('.fixed-gift');
   showModalByTime( '.popup-consultation', 5000);
 }
 
