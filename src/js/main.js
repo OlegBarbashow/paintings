6fff098724527a95906e20f5a4560b7f0d4a686b
Oleg Barbashow <teosofio@gmail.com>
2022-02-07 06:15:22 +0200

    Add module for working with sliders

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,12 @@
 import modals from './modules/modals';
+import sliders from "./modules/sliders";
 
 window.addEventListener('DOMContentLoaded', () =>{
    'use strict';
 
    modals();
+   sliders('.feedback-slider-item', 'horizontal', '.main-next-btn', '.main-prev-btn');
+   sliders('.main-slider-item', 'vertical');
 
 
 
