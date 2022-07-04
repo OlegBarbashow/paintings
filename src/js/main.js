53af0dbe049b25592ca5d68443424603078c02b7
Oleg Barbashow <teosofio@gmail.com>
2022-07-04 18:58:39 +0300

    Added image replacement on hover

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,7 @@ import showMoreStylesServer from "./modules/showMoreStylesServer";
 import calc from "./modules/calc";
 import filter from "./modules/filter";
 import pictureSize from "./modules/pictureSize";
+import accordion from "./modules/accordion";
 
 window.addEventListener('DOMContentLoaded', () =>{
    'use strict';
@@ -24,7 +25,7 @@ window.addEventListener('DOMContentLoaded', () =>{
    calc('#size', '#material', '#options', '.promocode', '.calc-price');
    filter();
    pictureSize('.sizes-block');
-
+   accordion('.accordion-heading', '.accordion-block');
 
 
 
