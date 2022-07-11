48c9aa04f105ef807e5314c38dd571328870d039
Oleg Barbashow <teosofio@gmail.com>
2022-07-11 08:22:07 +0300

    Added burger menu functionality

--- src/js/main.js
+++ src/js/main.js
@@ -9,6 +9,7 @@ import calc from "./modules/calc";
 import filter from "./modules/filter";
 import pictureSize from "./modules/pictureSize";
 import accordion from "./modules/accordion";
+import burger from "./modules/burger";
 
 window.addEventListener('DOMContentLoaded', () =>{
    'use strict';
@@ -26,6 +27,7 @@ window.addEventListener('DOMContentLoaded', () =>{
    filter();
    pictureSize('.sizes-block');
    accordion('.accordion-heading');
+   burger('.burger','.burger-menu', );
 
 
 
