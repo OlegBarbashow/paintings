3154db2aea10baa87b740fcbd61132e8d985c2d2
Oleg Barbashow <teosofio@gmail.com>
2022-07-23 08:13:42 +0300

    Added a smooth scroll on the site using the arrow to go to the beginning of the site

--- src/js/main.js
+++ src/js/main.js
@@ -10,6 +10,7 @@ import filter from "./modules/filter";
 import pictureSize from "./modules/pictureSize";
 import accordion from "./modules/accordion";
 import burger from "./modules/burger";
+import scrolling from "./modules/scrolling";
 
 window.addEventListener('DOMContentLoaded', () =>{
    'use strict';
@@ -23,12 +24,12 @@ window.addEventListener('DOMContentLoaded', () =>{
    checkTextInputs('[name = "message"]');
    // showMoreStyles('.button-styles', '.styles-2');
    showMoreStylesServer('.button-styles', '#styles .row');
-   calc('#size', '#material', '#options', '.promocode', '.calc-price');
+      calc('#size', '#material', '#options', '.promocode', '.calc-price');
    filter();
    pictureSize('.sizes-block');
    accordion('.accordion-heading');
    burger('.burger','.burger-menu', );
-
+   scrolling('.pageup');
 
 
 
